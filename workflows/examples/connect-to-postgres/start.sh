1a8c8e354811a4a80da46d251fde9c8de3f09093
Cezary Stanislawski <cezary.marek.stanislawski@gmail.com>
2024-02-10 13:02:40 +0100

    add script testing connection to postgres

---
+++ workflows/examples/connect-to-postgres/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Creating postgres-credentials secret"
+kubectl create secret generic postgres-credentials --from-literal=POSTGRES_USER="$(openssl rand -base64 12)" --from-literal=POSTGRES_PASSWORD="$(openssl rand -base64 12)" --from-literal=POSTGRES_DB=argo
+
+echo "Creating postgres resources"
+kubectl apply -f workflows/examples/connect-to-postgres/pg-svc-sts.yaml
+
+# wait for postgres to be ready
+kubectl wait --for=condition=ready --timeout=120s pod -l app=postgresql
+
+echo "Creating job testing connection to postgres"
+kubectl apply -f workflows/examples/connect-to-postgres/job.yaml
+
+kubectl wait --for=condition=complete --timeout=120s job/pg-connect
+
+echo "Creating workflow to connect to postgres"
+argo submit --watch workflows/examples/connect-to-postgres/workflow.yaml
